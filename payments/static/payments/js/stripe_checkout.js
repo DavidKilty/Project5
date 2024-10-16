499f1fc23787bd2492af03b6cec8ff5ac2c6a018
David Kilty <davidbkilty@gmail.com>
2024-10-16 15:47:12 +0000

    added to ticket list for better checkout

--- payments/static/payments/js/stripe_checkout.js
+++ payments/static/payments/js/stripe_checkout.js
@@ -2,6 +2,8 @@ document.querySelectorAll(".checkout-button").forEach(button => {
     button.addEventListener("click", function (event) {
         const ticketId = event.target.dataset.ticketId;
 
+        console.log(document.getElementById('stripe-publishable-key').textContent);
+
         fetch(`/create-checkout-session/?ticket_id=${ticketId}`, {
             method: "POST",
             headers: {
